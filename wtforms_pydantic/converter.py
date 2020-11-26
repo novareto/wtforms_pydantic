2f57c0da81bb49523d42d1acd78532a7982d9be2
Souheil Chelfouh <trollfot@gmail.com>
2020-11-27 00:07:13 +0100

    Cleanup and simplification

--- wtforms_pydantic/converter.py
+++ wtforms_pydantic/converter.py
@@ -1,108 +1,76 @@
-from pydantic import BaseModel
-from wtforms import validators
-from wtforms.fields.core import (
-    DecimalField, FloatField, IntegerField, StringField)
-from wtforms.fields.html5 import DateField, DateTimeField, TimeField
-from wtforms.fields.simple import TextAreaField
-from wtforms.fields import PasswordField
-from collections import OrderedDict, namedtuple
-from pydantic.types import SecretStr
 import datetime
 import decimal
+from collections import namedtuple
 
-FieldInfo = namedtuple("FieldInfo", ("name", "field"))
-
-
-default_converters = OrderedDict(
-    (
-        (str, StringField),
-        (int, IntegerField),
-        (float, FloatField),
-        (SecretStr, PasswordField),
-        (decimal.Decimal, DecimalField),
-        (datetime.date, DateField),  # WPDateField
-        (datetime.datetime, DateTimeField),  # WPDateTimeField
-        (datetime.time, TimeField),  # WPTimeField
-    )
-)
+import pydantic
+import wtforms.fields
+import wtforms.fields.html5
+import wtforms.validators
 
 
-class ModelConverter(object):
-    """convert entity attribute to Wtf-Field"""
+class Converter:
+    """convert pydantic fields into wtforms fields.
+    """
 
-    def __init__(self, additional=None, additional_coerce=None, overrides=None):
-        self.converters = {}
+    converters = {
+        str: wtforms.fields.StringField,
+        int: wtforms.fields.IntegerField,
+        float: wtforms.fields.FloatField,
+        decimal.Decimal: wtforms.fields.DecimalField,
+        datetime.date: wtforms.fields.html5.DateField,
+        datetime.datetime: wtforms.fields.html5.DateTimeField,
+        datetime.time: wtforms.fields.html5.TimeField,
+        pydantic.SecretStr: wtforms.fields.PasswordField
+    }
 
-        if additional:
-            self.converters.update(additional)
+    @classmethod
+    def convert(cls, fields: dict, **overrides):
 
-        # self.coerce_settings = dict( coerce_defaults )
-        # if additional_coerce:
-        #    self.coerce_settings.update(additional_coerce)
-        self.overrides = overrides or {}
+        wtfields = {}
+        for name, field in fields.items():
+            wtf_type = cls.converters.get(field.type_)
+            if wtf_type is None:
+                raise TypeError(
+                    f'No converter found for `{field.type_}.`')
 
-    def build_kwargs(self, entity, attr, field_args):
-        field_args = field_args or {}
-        kwargs = {
-            "label": attr.field_info.title or attr.name,
-            "validators": [],
-            "filters": [],
-            "default": attr.default,
-            'description': attr.field_info.description or '',
-        }
-        if field_args:
-            kwargs.update(field_args)
-        #  {"validators": [Email], "only_validators": True }
-        if not kwargs.pop("only_validators", False):
-            self.set_required_validator(attr, kwargs)
-            self.set_unique_validator(entity, attr, kwargs)
-        self.add_null_filter(attr, kwargs)
-        return kwargs
-
-    def set_required_validator(self, attr, kwargs):
-        if attr.required:
-            kwargs["validators"].append(validators.Required())
-        else:
-            kwargs["validators"].append(validators.Optional())
-            # further
-            if attr.default is not None:
-                kwargs["validators"].append(validators.Optional())
+            options = {
+                "label": field.field_info.title or name,
+                "validators": [],
+                "filters": [],
+                "default": field.default,
+                'description': field.field_info.description or ''
+            }
+            if field.required:
+                options["validators"].append(
+                    wtforms.validators.Required()
+                )
             else:
-                kwargs["validators"].append(ValueRequired())
+                options["validators"].append(
+                    wtforms.validators.Optional()
+                )
+                if field.nullable:
+                    # do something.
+                    pass
 
-    def set_unique_validator(self, entity, attr, kwargs):
-        return
-        if attr.is_unique:
-            kwargs["validators"].append(UniqueValidator(entity, attr.name))
+            if name in overrides:
+                options.update(overrides[name])
+            wtfields[name] = wtf_type(**options)
 
-    def add_null_filter(self, attr, kwargs):
-        return
-        if not attr.is_required and attr.nullable:
-            # Treat empty string as None when converting.
-            kwargs["filters"].append(handle_null_filter)
+        return wtfields
 
-    def get_custom(self, entity, attr, kwargs):
-        if attr.name in self.overrides:
-            self.is_filter_allowed(self.overrides[attr.name], kwargs)
-            return FieldInfo(attr.name, self.overrides[attr.name](**kwargs))
 
-    def convert(self, entity: BaseModel, attr, field_args):
-        kwargs = self.build_kwargs(entity, attr, field_args)
-        custom = self.get_custom(entity, attr, kwargs)
-        if custom:
-            return custom
-        # converter : { attr_type: fieldInfo }
+def model_fields(model, only=None, exclude=None) -> dict:
+    if not bool(only) ^ bool(exclude):
+        raise RuntimeError(
+            'You need to specify either `only` or `exclude`')
 
-        for converter in self.converters:
-            if isinstance(attr, converter):
-                # self.is_filter_allowed( self.converters[converter], kwargs)
-                return self.converters[converter](entity, attr, **kwargs)
-        try:
-            converter = default_converters[attr.type_]
-        except Exception:
-            raise AttributeError(
-                "There is not possible conversion for '%s' " % type(attr.type_)
-            )
+    if only:
+        return {
+            name: field for name, field in model.__fields__.items()
+            if name in only
+        }
 
-        # self.is_filter_allowed( converter, kwargs)
-        return FieldInfo(attr.name, converter(**kwargs))
+    return {
+        name: field for name, field in model.__fields__.items()
+        if name not in exclude
+    }
