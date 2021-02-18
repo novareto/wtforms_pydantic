df6b2ccb14e777a8a28f5a3481a1737e4d248553
Souheil CHELFOUH <trollfot@gmail.com>
2021-02-18 13:35:08 +0100

    Added readonly from wtforms_components and the Form class

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['wtforms', 'pydantic']
+requirements = ['wtforms', 'pydantic', 'wtforms_components']
 test_requires = ['pytest>=3', 'PyHamcrest']
 
 setup(
--- src/wtforms_pydantic/__init__.py
+++ src/wtforms_pydantic/__init__.py
@@ -3,3 +3,28 @@
 __author__ = """Christian Klinger"""
 __email__ = 'ck@novareto.de'
 __version__ = '0.1.0'
+
+
+import pydantic
+import wtforms.form
+from .converter import Converter, model_fields
+from wtforms_components import read_only
+
+
+class Form(wtforms.form.BaseForm):
+
+    _readonly: bool = False
+
+    @classmethod
+    def from_model(cls, model: pydantic.BaseModel,
+                   only=(), exclude=(), **overrides):
+        return cls(Converter.convert(
+            model_fields(model, only=only, exclude=exclude), **overrides
+        ))
+
+    def readonly(self):
+        if self._readonly:
+            return
+        for key, field in self._fields.items():
+            self._fields[key] = read_only(field)
+        self._readonly = True
